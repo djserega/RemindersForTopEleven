c0e9642616813e4d0b3083b70ab9fa5e40c2591c
djserega <galyuk.sergey@gmail.com>
2018-08-02 15:13:24 +0300

    UI reminder, send message

--- src/RemindersForTopEleven/MainWindow.xaml.cs
+++ src/RemindersForTopEleven/MainWindow.xaml.cs
@@ -22,31 +22,76 @@ namespace RemindersForTopEleven
     public partial class MainWindow : Window
     {
         private readonly ObservableCollection<Models.Match> _listOfMatches = new ObservableCollection<Models.Match>();
+        private readonly Reminders _reminders;
+        private readonly StartMatchEvents _startMatchEvents;
 
         public MainWindow()
         {
             InitializeComponent();
 
             DataContext = this;
+
+            _startMatchEvents = new StartMatchEvents();
+            _startMatchEvents.StartMatch += _startMatchEvents_StartMatch;
+            _reminders = new Reminders(_startMatchEvents);
         }
 
         public ObservableCollection<Models.Match> ListOfMatches { get => _listOfMatches; }
 
-        private void ButtonReadFile_Click(object sender, RoutedEventArgs e)
+        private void _startMatchEvents_StartMatch()
+        {
+            foreach (Models.Match item in GetListOfBeginningMatches())
+                SendMessageOnBeginingMatch(item);
+        }
+
+        private List<Models.Match> GetListOfBeginningMatches()
+        {
+            List<Models.Match> list = new List<Models.Match>();
+
+            TimeSpan timeSpanMax = new TimeSpan(0, 15, 0);
+            TimeSpan timeSpanMin = new TimeSpan(0, 0, 0);
+
+            foreach (Models.Match item in _listOfMatches)
+            {
+                TimeSpan currentTimeSpan = item.DateTime.Subtract(DateTime.Now);
+
+                if (timeSpanMax > currentTimeSpan && currentTimeSpan > timeSpanMin)
+                    list.Add(item);
+            }
+
+            return list;
+        }
+
+        private async void ButtonReadFile_Click(object sender, RoutedEventArgs e)
         {
             _listOfMatches.Clear();
 
-            new Reader().ReadData(_listOfMatches);
+            List<Models.Match> listOfMatches = await ReadDataAsync();
+            foreach (Models.Match item in listOfMatches)
+                _listOfMatches.Add(item);
         }
+
+        private async Task<List<Models.Match>> ReadDataAsync()
+            => await Task.Run(() => { return new Reader().ReadData(); });
 
         private void ButtonSendMessage_Click(object sender, RoutedEventArgs e)
         {
             if (DataGridListMatches.SelectedItem is Models.Match match)
-            {
-                string message = $"Скоро гра у команди {match.Team}. Початок о {match.DateTime.ToString("dd.MM HH:mm")}";
+                SendMessageOnBeginingMatch(match);
+        }
 
-                new TelegramApi().SendMessageAsync(message);
-            }
+        private void SendMessageOnBeginingMatch(Models.Match match)
+            => new TelegramApi().SendMessageAsync(match.GetMessageOnBeginingMatch());
+
+        private void ButtonStartTimer_Click(object sender, RoutedEventArgs e)
+        {
+            if (_listOfMatches.Count() > 0)
+                _reminders.StartTimer();
+        }
+
+        private void ButtonStopTimer_Click(object sender, RoutedEventArgs e)
+        {
+            _reminders.StopTimer();
         }
     }
 }
