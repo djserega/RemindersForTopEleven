7c1093002cde675ef0fed03d371dea2aa0690888
djserega <galyuk.sergey@gmail.com>
2018-08-02 15:12:12 +0300

    add class Reminders

---
+++ src/RemindersForTopEleven/Reminders.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RemindersForTopEleven
+{
+    internal class Reminders
+    {
+        private StartMatchEvents _startMatchEvents;
+        private bool _timerStopped;
+
+        internal Reminders(StartMatchEvents startMatchEvents)
+        {
+            _startMatchEvents = startMatchEvents;
+        }
+
+        internal async void StartTimer()
+        {
+            if (_timerStopped)
+                return;
+
+            await StartTimerAsync();
+            _startMatchEvents.EvokeStartMatch();
+            StartTimer();
+        }
+
+        internal void StopTimer()
+        {
+            _timerStopped = true;
+        }
+
+        private async Task StartTimerAsync()
+        {
+            await Task.Run(() => { Thread.Sleep(60 * 1000); });
+        }
+    }
+}
