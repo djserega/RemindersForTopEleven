4aa7e361b1439664ce6ad34973974e9e2f20ffd0
djserega <galyuk.sergey@gmail.com>
2018-08-02 15:12:00 +0300

    add events StartMatch

---
+++ src/RemindersForTopEleven/Events.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemindersForTopEleven
+{
+    internal delegate void StartMatch();
+
+    internal class StartMatchEvents : EventArgs
+    {
+        internal event StartMatch StartMatch;
+        internal void EvokeStartMatch() => StartMatch?.Invoke();
+    }
+}
